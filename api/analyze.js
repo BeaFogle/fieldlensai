ab780286759af4569b65c4e165c3ae3cc9404eaa
Beatrice Fogle <beafogle@gmail.com>
2026-06-25 17:16:38 -0400

    Add vercel.json to register API function

--- api/analyze.js
+++ api/analyze.js
@@ -1,6 +1,4 @@
-export const maxDuration = 30;
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -21,4 +19,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
